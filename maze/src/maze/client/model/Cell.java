312f1e7fa7b8f90776c7a6216ea9482b0749c922
Frank Grimm <grimmfrank@googlemail.com>
2015-06-12 13:14:11 +0200

    Added documentation.

--- maze/src/maze/client/model/Cell.java
+++ maze/src/maze/client/model/Cell.java
@@ -5,12 +5,15 @@ import java.util.Map;
 import maze.common.model.CellConnectionDirection;
 
 public interface Cell {
-
 	String id();
 
 	String name();
 
 	boolean isExit();
 
-	Map<CellConnectionDirection, Cell> neighbors();
+	/**
+	 * Contains the <em>ID</em> of each neighboring cell. Each neighbor is
+	 * mapped according to its {@link CellConnectionDirection direction}.
+	 */
+	Map<CellConnectionDirection, String> neighbors();
 }
